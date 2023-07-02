b0ff8f188132f8c60e036ae390572b23ab9dfbdb
saidurga <124700591+saidurgar@users.noreply.github.com>
2023-07-02 08:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	  return decimal.toString(2);
 }
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33));
+  
+
 
 window.decimalToBinary = decimalToBinary;
